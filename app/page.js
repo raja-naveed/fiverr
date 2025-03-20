0bc1221431e0eca3b91971c869eee8c05e47a732
Raja <rajadahri11@gmail.com>
2025-03-20 10:56:53 +0500

    Enhance homepage layout with swiper integration and responsive design adjustments

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import { IoIosArrowRoundForward } from "react-icons/io";
-import * as motion from "motion/react-client"
+import * as motion from "motion/react-client";
 
 const streamingPackages = [
   {
@@ -30,6 +30,24 @@ const streamingPackages = [
 ];
 
 export default function Home() {
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <div className="container mx-auto px-6 py-16">
       <p className="text-[16px] font-[400] text-[#D8261ACC] mb-4">
@@ -47,7 +65,7 @@ export default function Home() {
         </a>
       </div>
 
-      <div className="grid xl:grid-cols-1 ">
+      <div className="hidden lg:grid xl:grid-cols-1 md:grid-cols-1 sm:grid-cols-1 gap-6">
         {streamingPackages.map((pkg, index) => (
           <div
             key={index}
@@ -56,8 +74,8 @@ export default function Home() {
           >
             <div className="absolute inset-0 bg-[#3E4095] opacity-10 transform translate-y-full group-hover:translate-y-0 transition-transform duration-500 ease-in-out"></div>
 
-            <motion.div 
-            className="relative z-10 flex-shrink-0 w-[30%] mr-20"
+            <motion.div
+              className="relative z-10 flex-shrink-0 w-[30%] mr-20"
               initial={{ x: -200, opacity: 0 }}
               whileInView={{ x: 0, opacity: 1 }}
               transition={{ type: "spring", stiffness: 100, duration: 0.6 }}
@@ -69,7 +87,8 @@ export default function Home() {
               />
             </motion.div>
 
-            <motion.div className="relative z-10 flex-grow mx-14 w-[20%]"
+            <motion.div
+              className="relative z-10 flex-grow mx-14 w-[20%]"
               initial={{ y: -50, opacity: 0 }}
               whileInView={{ y: 0, opacity: 1 }}
               transition={{ duration: 0.5, delay: 0.1 }}
@@ -79,7 +98,8 @@ export default function Home() {
               </h3>
             </motion.div>
 
-            <motion.div className="relative z-10 flex-grow mx-14 w-[20%]"
+            <motion.div
+              className="relative z-10 flex-grow mx-14 w-[20%]"
               initial={{ y: -50, opacity: 0 }}
               whileInView={{ y: 0, opacity: 1 }}
               transition={{ duration: 0.5, delay: 0.1 }}
@@ -94,6 +114,21 @@ export default function Home() {
             </div>
           </div>
         ))}
+      </div>
+
+      {/* Mobile View Layout */}
+      <div className="lg:hidden block">
+          {streamingPackages.map((pkg, index) => (
+            <div key={index} className="flex flex-col mb-6 shadow-xl p-4">
+              <img
+                src={pkg.image}
+                alt={pkg.title}
+                className="w-full h-[208px] object-cover rounded-md mb-4"
+              />
+              <h3 className="text-[20px] font-[500] text-[#2A2A2A]">{pkg.title}</h3>
+              <p className="text-[14px] font-[400] text-black">{pkg.description}</p>
+            </div>
+          ))}
       </div>
     </div>
   );
